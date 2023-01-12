41747ddfc8e5109509700058cada8d2c868dc195
JohnnyGoodrich <jbgoodrich26@gmail.com>
2023-01-12 12:24:11 -0500

    components setup

---
+++ src/components/MealDetails.js
@@ -0,0 +1,12 @@
+import {useState, useEffect} from 'react';
+import {useParams} from 'react-router';
+import { Link } from "react-router-dom";
+
+function MealDetails(){
+
+    return (
+        <hi>Meal Details</hi>
+      )
+      
+}
+export default MealDetails

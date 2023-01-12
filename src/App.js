74f596b4fb647aa85645435d408e47e2ae28d171
cbowman422 <chrisb0wman@hotmail.com>
2023-01-12 13:45:56 -0500

    search by ingredient

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
-
-// COMPONENTS
-import MealList from "./components/MealList";
+import { Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import Home from "./components/Home.js"
+import MealList from "./components/MealList.js"
+import MealDetails from "./components/MealDetails.js"
 
 function App() {
 
+
+
+  const [searchBarItem, setSearchBarItem] = useState('')
+  const [isSearch, setIsSearch] = useState(false)
+  
+  
+  // Event handler for setting the search bar input to an event and saving that event to state.
+  const handleItemChange = (e) => 
+  {
+    const newSearchItem = e.target.value
+    setSearchBarItem(newSearchItem)
+  }
+  
+  // Function to change state and reset state to false after click search by name.
+  const onClick = () => 
+  {
+    setIsSearch(current => !current)
+    if (setIsSearch) 
+    {
+      setTimeout(function() 
+      {
+        setIsSearch(current => !current)
+      }, 1);
+    } 
+    else 
+    {
+    }
+  }
+  
+  
+
+
+
   return (
     <div className="App">
-      <Header />
+            <div className="searchBar">
+          <input type="text" id="myInput" value={searchBarItem} onChange={handleItemChange} placeholder='Search..'/>
+            <Link to={`/drinks-details/${searchBarItem}`}> 
+              <button onClick={onClick} className='searchName' > by ingredient </button>
+            </Link>
+      </div>
       <h1>hi</h1>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
         <Route path="/list" element={<MealList />} />
-        <Route path="/details" />
+        <Route path="/details" element={<MealDetails  drinkName={searchBarItem} isSearch={isSearch} /> } />
       </Routes>
     </div>
   );

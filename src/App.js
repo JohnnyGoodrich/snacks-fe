2da3c9075db984773b30cf0d2a52fe745d1eb605
cbowman422 <chrisb0wman@hotmail.com>
2023-01-12 13:12:30 -0500

    merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 function App() {
+
   return (
     <div className="App">
+      <Header />
       <h1>hi</h1>
     </div>
   );

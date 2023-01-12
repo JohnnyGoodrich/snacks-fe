b56bd86b3fff95d71d36311fc721739896214277
JohnnyGoodrich <jbgoodrich26@gmail.com>
2023-01-12 13:26:04 -0500

    fixed meail details page in development

--- src/components/MealDetails.js
+++ src/components/MealDetails.js
@@ -5,19 +5,19 @@ import { Link } from "react-router-dom";
 function MealDetails(){
     const id = useParams()
     const url = `www.themealdb.com/api/json/v1/1/lookup.php?i=${id.id}`
-    const [drinkDetails, setDrinkDetails]=useState(null)
+    const [mealDetails, setMealDetails]=useState(null)
     useEffect( ()=> {
         fetch(url)
             .then((response)=>response.json())
             .then((json)=> {
-                setDrinkDetails(json)
+                setMealDetails(json)
           
         })
             .catch(console.error)
     }, [])
     return (
 
-        drinkDetails ?
+        mealDetails ?
         <div>
 
         </div> :<p>Loading...</p>

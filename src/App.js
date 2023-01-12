2e0d366613687abeea2f097b5407f2ab49bc604a
npereznyc <natalieperez84@gmail.com>
2023-01-12 14:20:07 -0500

    Home.jsx rendering ingredient names and images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>hi</h1>
+      
     </div>
   );
 }

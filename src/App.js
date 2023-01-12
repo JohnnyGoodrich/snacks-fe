293f816b5767ca471666c953b20d5cef55f82791
JohnnyGoodrich <jbgoodrich26@gmail.com>
2023-01-12 11:55:12 -0500

    initial app setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default App;

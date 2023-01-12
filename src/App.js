6ce7cdaa2fb3511c07c06fda663e1786f416602a
JohnnyGoodrich <jbgoodrich26@gmail.com>
2023-01-12 13:15:07 -0500

    getting routes from Luigi branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+
+
+// COMPONENTS
+import MealList from "./components/MealList";
 
 function App() {
   return (
     <div className="App">
       <h1>hi</h1>
+      <Routes>
+        <Route path="/" />
+        <Route path="/list" element={<MealList />} />
+        <Route path="/details/:id" />
+      </Routes>
     </div>
   );
 }
